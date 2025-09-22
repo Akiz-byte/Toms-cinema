71ac7dc9dc247cb31fb093c0ce9ac6f743c919e6
Akshay Sawant <akshaysavant0@gmail.com>
2025-09-22 09:58:08 +0530

    Refactor code to add movies links

--- index.js
+++ index.js
@@ -10,16 +10,19 @@ document.addEventListener("click", (e)=>{
         if(!watchlist.find(item => item.imdbID === movie.imdbID)){
             watchlist.unshift(movie)
             localStorage.setItem("watchlist", JSON.stringify(watchlist))
-            document.querySelector(`#watchlist-button.${movie.imdbID}`).innerHTML = `<span id="added-watchlist"><i class="fa-solid fa-circle-check fa-xl"> </i>Added</span>`
-            alert("Movie added to watchlist")
-            const vidsrcUrl = `https://vidsrc.xyz/embed/movie?imdb=${encodeURIComponent(movie.imdbID)}`
-            window.open(vidsrcUrl, '_blank', 'noopener')
-
+            document.querySelector(`#watchlist-button.${movie.imdbID}`).innerHTML = `<span id="added-watchlist"><i class="fa-solid fa-circle-check fa-xl"> </i> Added</span>`
         }else{
-            alert("Movie is already in watchlist")
         }
     }
-    })
+
+    else if(e.target.dataset.imdbid){
+        const movie = searchResult.find(movie => movie.imdbID === e.target.dataset.imdbid)
+        if(movie){
+            const vidsrcUrl = `https://vidsrc.xyz/embed/movie?imdb=${encodeURIComponent(movie.imdbID)}`
+            window.open(vidsrcUrl, '_blank', 'noopener')
+        }   
+    }
+})
 
 searchInput.addEventListener("keydown", function(e){
     if(e.key === "Enter"){
@@ -30,54 +33,70 @@ searchInput.addEventListener("keydown", function(e){
 searchBtn.addEventListener("click", function(){
     fetch(`https://www.omdbapi.com/?apikey=511297ae&s=${searchInput.value}`)
     .then(res => res.json())
-    .then( data =>{
-        console.log(data)
-        if(data.Response==="True"){
-            let searchResultHtml = []
-                data.Search.forEach(data =>{
-                    fetch(`https://www.omdbapi.com/?apikey=511297ae&i=${data.imdbID}`)
+    .then(data => {
+        if (data.Response === "True") {
+            let searchResultHtml = [];
+            data.Search.forEach(movie => {
+                fetch(`https://www.omdbapi.com/?apikey=511297ae&i=${movie.imdbID}`)
                     .then(res => res.json())
-                    .then(data =>{
-                        searchResult.unshift(data)
-                        searchResultHtml = searchResult.map(data=>{
+                    .then(movieDetails => {
+                        searchResult.unshift(movieDetails)
 
-                            // fetch(`https://vidsrc.xyz/embed/movie?imdb=tt5433140`)
-                            // .then(res => res.json())
-                            // .then(data => console.log(data))
-                            return `<div class="movie-list" id="movie-list">
-                                <div id="movie-poster">
-                                    <img src=${data.Poster} alt="">
-                                </div>
-                            <div class="movie-details">
-                                <div id="movie-title">
-                                    <h2>${data.Title}</h2>
-                                    <p>⭐ ${data.imdbRating}</p>
-                                </div>
-                                <div id="movie-description">
-                                    <p id="length">${data.Runtime}</p>
-                                    <p id="genre">${data.Genre}</p>
-                                    <div id="watchlist-button" class="${data.imdbID}">
-
-                                    </div>
-                                </div>
-                                <div id="movie-content">${data.Plot}</div>
-                            </div>
-                            </div>
-                    `
-                            })
-                            
-                            
-                            document.getElementById("search-result").innerHTML = searchResultHtml.join('')
-                            searchResult.forEach(movie =>{
-                                if(watchlist.find(item => item.imdbID === movie.imdbID)){
-                                    document.querySelector(`#watchlist-button.${movie.imdbID}`).innerHTML = `<span id="added-watchlist"><i class="fa-solid fa-circle-check fa-xl"> </i>Already Added</span>`
-                                }else{
-                                    document.querySelector(`#watchlist-button.${movie.imdbID}`).innerHTML = `<button id="add-watchlist" data-addwatchlist="${movie.imdbID}"><i class="fa-solid fa-circle-plus fa-xl"></i> Watchlist</button>`
+                        const tmdbApiKey = "0289f710200880e871f592ad0ea20d41"
+                        const tmdbSearchUrl = `https://api.themoviedb.org/3/find/${movieDetails.imdbID}?api_key=${tmdbApiKey}&external_source=imdb_id`;
+                        fetch(tmdbSearchUrl)
+                            .then(tmdbRes => tmdbRes.json())
+                            .then(tmdbData => {
+                                console.log(tmdbData)
+                                const tmdbMovie = tmdbData.movie_results && tmdbData.movie_results[0];
+                                if (!tmdbMovie) {
+                                    appendMovie(movieDetails, null);
+                                    return;
                                 }
-                                searchInput.value = ""
+
+                                fetch(`https://api.themoviedb.org/3/movie/${tmdbMovie.id}/videos?api_key=${tmdbApiKey}`)
+                                    .then(trailerRes => trailerRes.json())
+                                    .then(trailerData => {
+                                        console.log(trailerData)
+                                        const trailer = trailerData.results && trailerData.results.find(
+                                            video => video.site === "YouTube" && video.type === "Trailer"
+                                        );
+                                        appendMovie(movieDetails, trailer ? `https://www.youtube.com/watch?v=${trailer.key}` : null)
+                                    })
+                                    .catch(() => appendMovie(movieDetails, null))
                             })
-                    })
-                })
+                            .catch(() => appendMovie(movieDetails, null))
+                    });
+            });
+
+            function appendMovie(data, trailerUrl) {
+                searchResultHtml.unshift(
+                    `<div class="movie-list" id="movie-list">
+                        <div id="movie-poster">
+                            <img src="${data.Poster}" alt="${data.Title}">
+                        </div>
+                        <div class="movie-details">
+                            <div id="movie-title">
+                                <h2>${data.Title}</h2>
+                                <p>⭐ ${data.imdbRating}</p>
+                            </div>
+                            <div id="movie-description">
+                                <p id="length">${data.Runtime}</p>
+                                <p id="genre">${data.Genre}</p>
+                                <div id="watchlist-button" class="${data.imdbID}"></div>
+                            </div>
+                            <div id="movie-content">${data.Plot}</div>
+                        </div>
+                        <div class="watch-video">
+                            <button class="watch-video-btn" data-imdbid="${data.imdbID}">
+                                <i class="fa-solid fa-play fa-xl"></i> Watch
+                            </button>
+                            ${trailerUrl ? `<a href="${trailerUrl}" target="_blank" class="trailer-btn">Watch Trailer</a>` : `<span class="no-trailer">Trailer Not Available</span>`}
+                        </div>
+                    </div>`
+                );
+                document.getElementById("search-result").innerHTML = searchResultHtml.join('');
+            }
         }
         if(data.Response==="False"){
             document.getElementById("no-state").classList.add("hide") 
--- watchlist.js
+++ watchlist.js
@@ -1,11 +1,17 @@
 let data = JSON.parse(localStorage.getItem("watchlist"))
 
 document.addEventListener("click", (e)=>{
-    if(e.target.dataset.remove){
-        const movieId = e.target.dataset.remove
-        data.splice(data.findIndex(movie => movie.imdbID === movieId), 1)
+    if(e.target.dataset.remove){ 
+        data.splice(data.findIndex(movie => movie.imdbID === e.target.dataset.remove), 1)
         localStorage.setItem("watchlist", JSON.stringify(data))
         renderWatchList()
+    }
+    if(e.target.dataset.imdbid){
+        const movie = data.find(movie => movie.imdbID === e.target.dataset.imdbid)
+        if(movie){ 
+        const vidsrcUrl = `https://vidsrc.xyz/embed/movie?imdb=${encodeURIComponent(movie.imdbID)}`
+        window.open(vidsrcUrl, '_blank', 'noopener')
+        }
     }
 })
 
@@ -14,9 +20,9 @@ function renderWatchList(){
 let watchlistHTML = []
 watchlistHTML = data.map(movie =>{
     return `<div class="movie-list" id="movie-list">
-                    <div id="movie-poster">
-                        <img src=${movie.Poster} alt="">
-                    </div>
+                <div id="movie-poster">
+                    <img src=${movie.Poster} alt="">
+                </div>
                 <div class="movie-details">
                     <div id="movie-title">
                         <h2>${movie.Title}</h2>
@@ -25,11 +31,16 @@ watchlistHTML = data.map(movie =>{
                     <div id="movie-description">
                         <p id="length">${movie.Runtime}</p>
                         <p id="genre">${movie.Genre}</p>
-                        <button id="remove-watchlist" data-remove="${movie.imdbID}"><i class="fa-solid fa-circle-minus fa-xl"></i> Remove</button>
+                        <button id="remove-watchlist" class="add-remove-watchlist" data-remove="${movie.imdbID}"><i class="fa-solid fa-circle-minus fa-xl"></i> Remove</button>
                     </div>
                     <div id="movie-content">${movie.Plot}</div>
                 </div>
+                <div class="watch-video">
+                    <button class="watch-video-btn" data-imdbid="${movie.imdbID}">
+                        <i class="fa-solid fa-play fa-xl"></i> Watch
+                    </button>
                 </div>
+            </div>
         `
 
 }).join('') ;
@@ -43,5 +54,71 @@ if(data.length === 0){
     </div>`
 }
 }
+let watchlistHTML = [];
+let pending = data.length;
+const tmdbApiKey = "0289f710200880e871f592ad0ea20d41"
 
-renderWatchList()
+function getTrailerUrl(imdbID, cb) {
+    const tmdbSearchUrl = `https://api.themoviedb.org/3/find/${imdbID}?api_key=${tmdbApiKey}&external_source=imdb_id`;
+    fetch(tmdbSearchUrl)
+        .then(res => res.json())
+        .then(tmdbData => {
+            const tmdbMovie = tmdbData.movie_results && tmdbData.movie_results[0];
+            if (!tmdbMovie) return cb(null);
+            const tmdbMovieId = tmdbMovie.id;
+            const trailerUrl = `https://api.themoviedb.org/3/movie/${tmdbMovieId}/videos?api_key=${tmdbApiKey}`;
+            fetch(trailerUrl)
+                .then(res => res.json())
+                .then(trailerData => {
+                    const trailer = trailerData.results && trailerData.results.find(
+                        video => video.site === "YouTube" && video.type === "Trailer"
+                    );
+                    cb(trailer ? `https://www.youtube.com/watch?v=${trailer.key}` : null);
+                })
+                .catch(() => cb(null));
+        })
+        .catch(() => cb(null));
+}
+
+if (data.length === 0) {
+    document.getElementById("search-result").innerHTML = `<div class="no-state" id="no-state-watchlist">
+        <p>Your watchlist is looking a little empty...</p>
+        <a href="index.html"><button id="add-movies"><i class="fa-solid fa-circle-plus fa-2xl"></i> Let's add some movies!</button></a>
+    </div>`;
+}
+
+data.forEach(movie => {
+    getTrailerUrl(movie.imdbID, function(trailerUrl) {
+        watchlistHTML.push(`
+            <div class="movie-list" id="movie-list">
+                <div id="movie-poster">
+                    <img src=${movie.Poster} alt="">
+                </div>
+                <div class="movie-details">
+                    <div id="movie-title">
+                        <h2>${movie.Title}</h2>
+                        <p>⭐ ${movie.imdbRating}</p>
+                    </div>
+                    <div id="movie-description">
+                        <p id="length">${movie.Runtime}</p>
+                        <p id="genre">${movie.Genre}</p>
+                        <button id="remove-watchlist" class="add-remove-watchlist" data-remove="${movie.imdbID}"><i class="fa-solid fa-circle-minus fa-xl"></i> Remove</button>
+                    </div>
+                    <div id="movie-content">${movie.Plot}</div>
+                </div>
+                <div class="watch-video">
+                    <button class="watch-video-btn" data-imdbid="${movie.imdbID}">
+                        <i class="fa-solid fa-play fa-xl"></i> Watch
+                    </button>
+                    ${trailerUrl ? `<a href="${trailerUrl}" target="_blank" class="trailer-btn">Watch Trailer</a>` : `<span class="no-trailer">Trailer Not Available</span>`}
+                </div>
+            </div>
+        `);
+        pending--;
+        if (pending === 0) {
+            document.getElementById("search-result").innerHTML = watchlistHTML.join('');
+        }
+    });
+});
+
+renderWatchList();
